70406bff2bd9742a1e29532b35fd8f4e924fa5b2
kachoko <aykato@enceladus.co.jp>
2023-11-30 16:25:59 +0900

    検証画面

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,82 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useEffect, useState } from "react";
+
+const OsName = {
+    ANDROID: "Android",
+    IOS: "iOS",
+    IPADOS: "iPadOS",
+};
+
+const getOsType = () => {
+    const UA = navigator.userAgent;
+    switch (true) {
+        case /android/i.test(UA):
+            return OsName.ANDROID;
+        case /iphone/i.test(UA):
+            return OsName.IOS;
+        case /macintosh/i.test(UA) && navigator.maxTouchPoints > 0:
+            return OsName.IPADOS;
+        default:
+            return UA;
+    }
+};
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+    const [isModal, setIsModal] = useState(false);
+    let installPromptEvent: any;
+
+    useEffect(() => {
+        window.addEventListener("beforeinstallprompt", (event) => {
+            // Chrome67以前で自動的にプロンプトを表示しないようにする?
+            event.preventDefault();
+            // イベントを変数に保存する
+            installPromptEvent = event;
+        });
+    }, []);
+
+    const installApp = () => {
+        console.log("install!");
+        // ホーム画面に追加のダイアログを表示させる
+        installPromptEvent.prompt();
+
+        // ダイアログの結果をプロミスで受け取る
+        installPromptEvent.userChoice.then((choice: any) => {
+            if (choice.outcome === "accepted") {
+                console.log("User accepted the A2HS prompt");
+            } else {
+                console.log("User dismissed the A2HS prompt");
+            }
+            // Update the install UI to notify the user app can be installed
+            installPromptEvent = null;
+        });
+    };
+
+    const isPwaMode = () => {
+        if (window.matchMedia("(display-mode: standalone)").matches) {
+            // ここにPWA環境下でのみ実行するコードを記述
+            return true;
+        }
+        return false;
+    };
+
+    return (
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <p>検証用コンポーネント</p>
+            <div>
+                <p>OSの取得</p>
+                <span>OS: {getOsType()}</span>
+            </div>
+            <div>
+                <p>PWAであるかどうかの取得</p>
+                <span>
+                    現在PWAモードで
+                    {isPwaMode() ? `動作しています` : `動作していません`}
+                </span>
+            </div>
+            {/* {getOsType() === OsName.ANDROID && (
+                <button onClick={installApp}>ホームにインストール</button>
+            )} */}
+            <button onClick={installApp}>ホームにインストール</button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
